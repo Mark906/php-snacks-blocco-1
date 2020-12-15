cd620d2fe2854721036c1bc6c298e12ce54c1208
Marco <markoka1@outlook.it>
2020-12-15 23:15:53 +0100

    creazione array

---
+++ phpsnack1/index.php
@@ -0,0 +1,44 @@
+<?php
+    $matches = [
+        [
+            'squadra1' => 'Milano',
+            'squadra2' => 'Matera',
+            'puntisq1' =>  80,
+            'puntisq2' => 90
+        ],
+        [
+            'squadra1' => 'Firenze',
+            'squadra2' => 'Roma',
+            'puntisq1' =>  66,
+            'puntisq2' => 73
+        ],
+        [
+            'squadra1' => 'Bologna',
+            'squadra2' => 'Arezzo',
+            'puntisq1' =>  54,
+            'puntisq2' => 78
+        ],
+        [
+            'squadra1' => 'Torino',
+            'squadra2' => 'Catanzaro',
+            'puntisq1' =>  64,
+            'puntisq2' => 82
+        ],
+        [
+            'squadra1' => 'Padova',
+            'squadra2' => 'Isernia',
+            'puntisq1' =>  80,
+            'puntisq2' => 90
+        ],
+    ]
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+
+    </body>
+</html>
